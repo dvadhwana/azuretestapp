333693829f718bc69aec78657dd4bdbf4040700d
Divyesh Vadhwana <dvadhwana@ingrooves.com>
2019-01-08 18:13:03 +0530

    Fixed AB#1

--- azuretestapp/Service/APODService.cs
+++ azuretestapp/Service/APODService.cs
@@ -19,13 +19,15 @@ namespace azuretestapp.Service
         }
 
         public virtual async Task<JObject> GetAPOD (DateTime APODdate)
-        {
-            string uri = "https://api.nasa.gov/planetary/apod?api_key=HfNsuJpUXQplm4uGYogg5oMe5JnW7EhdN5Ke3seP&&start_date=" + APODdate.ToString("yyyy-MM-dd") + "&end_date=" + APODdate.ToString("yyyy-MM-dd");
+        {            
+            string uri = "https://api.nasa.gov/planetary/apod?api_key=HfNsuJpUXQplm4uGYogg5oMe5JnW7EhdN5Ke3seP&start_date=" + APODdate.ToString("yyyy-MM-dd") + "&end_date=" + APODdate.ToString("yyyy-MM-dd");
+            _logger.LogInformation("Before calling Nasa API @ URL : " + uri);
             try
             {
                 var response = await _client.GetAsync(uri);
                 JObject JsonObject = JObject.Parse("{ APODS : " + response.Content.ReadAsStringAsync().Result + " }");
-                return JsonObject;
+                _logger.LogInformation("Call to Nasa API successful");
+                return JsonObject;                
             }
             catch (Exception ex)
             {
